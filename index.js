dbcff074c2273258fcf25a708fe0fae510279ea0
JesusAParedes <paredesjesus93@gmail.com>
2023-01-15 16:45:25 -0600

    wrote in the paths with the changes

--- index.js
+++ index.js
@@ -8,33 +8,47 @@ const { users } = require('./state')
 app.use(express.json())
 
 /* BEGIN - create routes here */
-app.get('/users', (req, res) => {
-  res.json(users);
+
+app.get('/users', (req, res) => res.json(users))
+
+app.get('/users/:userId', (req, res) => {
+  res.json(users.filter(user => user._id === parseInt(req.params.userId)))
+  })
+
+
+app.post('/users', (req, res) => {
+  const newUser = {
+    _id: users[users.length - 1]._id + 1,
+    "name": req.body.name,
+    "occupation": req.body.occupation,
+    "avatar": req.body.avatar
+  }
+  users.push(newUser);
+  return res.json(newUser);
 })
 
-app.get('/users/:id', (req, res) => {
-  let humanId = req.params.id;
-  users.filter(user => {
-    if(user['_id'] == humanId) {
-    res.json(user)};
+app.put('/users/:userId', (req, res) => {
+  const updateUser = req.body;
+  users.forEach(user => {
+    if(user._id === parseInt(req.params.userId)) {
+      user.name = updateUser.name ? updateUser.name : updateUser.name,
+      user.occupation = updateUser.occupation ? updateUser.occupation : updateUser.occupation
+      // user.avatar = updateUser.avatar ? updateUser.avatar : updateUser.avatar;
+      return res.json(user);
+    }
   })
 })
 
-app.post('/users', (req, res) => {
-  let data = {
-    "_id": 7,
-    "name": "John Jacob",
-    "occupation": "Pumpkin Hunter",
-    "avatar": "http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337"
-  };
-
-  res.json(users.push(data));
-})
-
-// app.put('/users/1', (req, res) => {
-//   res.json(users)
-// })
-
+app.delete('/users/:userId', (req, res) => {
+  res.json(
+    { Message: 'User deleted',
+    users: users.filter(user => {
+    if(user._id !== parseInt(req.params.userId)) {
+       return user;
+    }
+    if(user._id === parseInt(req.params.userId)) {
+       user.isActive = false;
+    }})})})
 /* END - create routes here */
 
 app.listen(port, () => 
